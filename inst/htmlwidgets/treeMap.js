b39415b9b31cea4763089c1b4db3b3197ddd8b1b
adymimos <adline.dsilva@mimos.my>
2015-08-26 15:37:03 +0800

    Update treeMap.js

--- inst/htmlwidgets/treeMap.js
+++ inst/htmlwidgets/treeMap.js
@@ -1,3 +1,7 @@
+var count = 0;
+ var val, currentName, setInter, calcHeight, marginTop = undefined;
+
+ var arr = new Array;
 HTMLWidgets.widget({
 
   name: 'treeMap',
@@ -7,7 +11,7 @@ HTMLWidgets.widget({
   renderOnNullValue: true,
 
 initialize: function(el, width, height) {
-
+  
   var tm = new $jit.TM.Squarified({
     //where to inject the visualization
     injectInto: el.id,
@@ -19,13 +23,7 @@ initialize: function(el, width, height) {
     animate: true,
     //box offsets
     offset: 1,
-    //use canvas text
-//    Label: {
-  //    type: 'HTML',
-    //  size: 20,
-    //  color:'#ccc'
-   //   family: 'Tahoma, Verdana, Arial'
-//    },
+
 Label : {  
   overridable: false,  
   type: 'HTML', //'SVG', 'Native'  
@@ -49,21 +47,70 @@ Label : {
     Events: {
       enable: true,
       onClick: function(node) {
+
+        clearInterval(setInter);
+        
+        calcHeight = $("#"+node.id.toString()+" span").css('top');
+        
+        //var newFieldName = 'my new field name';
+        //var newFieldValue = 'my new field value';
+        
+var obj = {id : node.id.toString(), top:calcHeight};
+
+arr.push({id: obj.id, top: obj.top});
+    
         if(node) 
-        {
-        	var click='{ "treemap" : [' +
-                '{ "type":"onMouseEnter" , "node":"'+node.name+'" }]}';
-                Shiny.onInputChange(el.id+'_treemap_event',click);
-         	tm.enter(node);
-        }
-        
-        
+	      {
+          
+          setInter = setInterval(function(){ 
+            $("#tree-label div:visible:first").addClass("titleRoot");
+          
+            $("#tree-label div:visible:first span").css({"display":"block", "top":"0px"});
+            $(".titleRoot span").css({"width": "500px", "height":"30px"});
+            
+          }, 1000);
+          
+          
+         //setInterval($("#tree-label div:visible:first").click, 500);
+       
+		  tm.enter(node);
+
+    
+  	var click='{ "treemap" : [' +
+		'{ "type":"onMouseEnter" , "node":"'+node.name+'" }]}';
+    
+          Shiny.onInputChange(el.id+'_treemap_event',click);
+          console.log(click);
+          console.log(el.id+'treemap_event');
+          
+                  
+	}
       },
-      onRightClick: function() {
-      	var click='{ "treemap" : [' +
-                '{ "type":"onMouseLeave" , "node":"'+tm.clickedNode.name+'" }]}';
-                        Shiny.onInputChange(el.id+'_treemap_event',click);
+      onRightClick: function(node) {
+        
+        
+         clearInterval(setInter);
+  
+        for(var i=0; i<arr.length; i++){
+          //console.log(arr[i].id, arr[i].top);
+          if(tm.clickedNode.id.toString() == arr[i].id){
+            $("#tree-label div:visible:first span").animate({ 'top': arr[i].top}, 1000);
+          }
+        }
+
+        
+       $("#tree-label div:visible:first span").css("display","table-cell");
         tm.out();
+        
+
+		var click='{ "treemap" : [' +
+		'{ "type":"onMouseLeave" , "node":"'+tm.clickedNode.name+'" }]}';
+                        Shiny.onInputChange(el.id+'_treemap_event',click);
+                	console.log(click);
+                  
+ $("#tree-label").find(":visible:first").removeClass('titleRoot');
+  //$(".node span").css("display","table-cell !important");
+	
       },
       onMouseEnter: function(node, eventInfo) {
         if(node) {
@@ -75,7 +122,8 @@ Label : {
       },
       onMouseLeave: function(node) {
         if(node) {
-          node.removeCanvasStyle('shadowBlur');
+          
+//          node.removeCanvasStyle('shadowBlur');
           tm.plot();
         }
       }
@@ -93,45 +141,136 @@ Label : {
       onShow: function(tip, node, isLeaf, domElement) {
         var html = "<div class=\"tip-title\">" + node.name 
           + "</div><div class=\"tip-text\">";
-            var data = node.data;  
+            var data = node.data;
+            
        if(data.cases) {  
-        html +=  data.cases;  
+        html +=  data.cases;
+        
       }  
-        tip.innerHTML =  html; 
+        tip.innerHTML =  html;
+        
       }  
       
     },
     //Add the name of the node in the correponding label
     //This method is called once, on label creation.
+    
     onCreateLabel: function(domElement, node){
-        domElement.innerHTML = node.name;
+      
+        domElement.innerHTML = "<span>"+node.name+"</span>";
         var style = domElement.style;
         style.display = '';
-        style.fontSize= '1.2vw';
+        //style.fontSize= '1.2vw';
+        var divContent = domElement;
+        var divId = domElement.id;
+        var divId1 = domElement.innerHTML;
+        count++;
+        var nodeHeight = node.data.$height;
+        var nodeWidth = node.data.$width;
+        //console.log(node.endData.$width);
+        val = Math.ceil(node.endData.$width);
+        valH = Math.ceil(node.endData.$height);
+        
+        
         if(!tm.leaf(node))
          {
+          
           style.border = '2px solid #0099FF';  
          }
         else
         {
+          
           style.border = '1px solid transparent';  
         }
         
+
+        
         domElement.onmouseover = function() {
           style.border = '2px solid gray';
+
         };
+        
         domElement.onmouseout = function() {
            if(!tm.leaf(node))
             {
+              
               style.border = '2px solid #0099FF';  
             }
             else
             {
+              
               style.border = '1px solid transparent';  
             }
+
         };
+         
+
+/*
+	$jit.util.addEvent(domElement, 'mouseup', function (event) {
+   	// detecting right button
+	   if (event.button != 2) {
+	       return;
+	   }
+		 var click='{ "treemap" : [' +
+                '{ "type":"onMouseLeave" , "node":"'+node.name+'" }]}';
+                        Shiny.onInputChange('treemap_event',click);
+                        console.log(click);
+
+	}); */
+  
+
+        
+     if(node.id.toString() == "root"){
+          style.fontSize = "20px";
+        }else{
+          
+          //style.fontSize = val/8+"px";  
+          if(node.endData.$width <= 100) {
+          //console.log(node.endData.$width);
+          style.fontSize = "16px";
+        }else{
+        style.fontSize = val/8+"px";  
+        }
+        
+        var counts = 0;
+      var listOfNode = setInterval(function(){
+        
+        if(counts >=1){
+          clearInterval(listOfNode);
+        }else{
+        $.each(node.id, function(i, j){
+         
+         $("#"+j+" span").css({
+          'width':Math.ceil(node.endData.$width)+"px",
+          'height':Math.ceil(node.endData.$height) + "px",
+          'position':'relative',
+          'margin': 'auto'
+        });
+        
+        $("#"+j).css({
+        'fontSize': Math.ceil(node.endData.$width) / 8+"px"
+        });
+        
+        marginTop = Math.ceil(node.endData.$height)/2.5;
+        var fontSize = $("#"+node.id.toString());
+               
+        //$("#"+node.id.toString().css("font-size",fontSize / 8+"px"));
+        //console.log(marginTop);
+        
+         $("#"+j+" span").animate({ 'top': marginTop +'px' }, 1000);
+        
+        });
+        counts++;
+        
+        };},1000);
+          
+        }
+        
+        
     }
   });
+  
+
 
 	return {
 	tree: tm
@@ -149,19 +288,36 @@ Label : {
   var tp = x.tooltip;
 
   tm.loadJSON(json);
+  
   tm.refresh();
-  tm.refresh();
+
+  //console.log(json);
   instance.lastValue = x;
   
+
   },
 
   resize: function(el, width, height, instance) {
     // Re-render the previous value, if any
    var tm = instance.tree;
+
    tm.config.animate = false;
    tm.canvas.resize(width, height); // resize the treemap to fit the background container
    tm.config.animate = true;
 
+//   this.renderValue(el, instance.lastValue, instance);
   }
 
 });
+
+$(function(){
+$("<style>")
+.prop("type", "text/css")
+.html("\
+.titleRoot{\
+font-size:20px !important; }")
+.appendTo("head");
+    
+});
+
+
